87de912ed21663cc95e73bd04af934469d20dc15
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-03-16 16:29:24 +0900

    Top
    Topのコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <Header></Header>
+    <Top></Top>
     <About></About>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Top from './components/Top.vue'
 import About from './components/About.vue'
 
 
 export default {
   components: {
     Header,
+    Top,
     About,
   }
 }
