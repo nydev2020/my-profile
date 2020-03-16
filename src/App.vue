bc88289aa769fa0c5e6da3288d3b8c01a10efd96
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-03-16 16:23:22 +0900

    About
    Aboutコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <Header></Header>
+  <div>
+    <Header></Header>
+    <About></About>
+  </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import About from './components/About.vue'
+
 
 export default {
   components: {
     Header,
+    About,
   }
 }
 </script>
+
+
 
 <style>
 </style>
